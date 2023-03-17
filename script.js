738da6e298764c459108cfe936690627665ac22c
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-03-16 17:52:33 -0700

    Created JS logic for hiding start window

---
+++ script.js
@@ -0,0 +1,7 @@
+//Front Screen Disappear On Click
+document.getElementById('start').addEventListener("click", hide);
+
+
+function hide() {
+    document.getElementById('startingpage').style.visibility = 'hidden';
+}
