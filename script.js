bb255e92fba49a3371adc8ea1081f777409ac05d
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-03-17 22:11:00 -0700

    Added array of objects for test questions

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ document.getElementById('start').addEventListener("click", hide);
 function hide() {
     document.getElementById('startingpage').style.visibility = 'hidden';
 }
+
+const quizq1 = [
+    {
+        question: "Question 1",
+        options: ["1","2","3","4"],
+        correct: "2",
+    },
+    {
+        question: "Question 2",
+        options: ["1","2","3","4"],
+        correct: "3",
+    },
+    {
+        question: "Question 2",
+        options: ["1","2","3","4"],
+        correct: "3",
+    },{
+        question: "Question 2",
+        options: ["1","2","3","4"],
+        correct: "3",
+    },{
+        question: "Question 2",
+        options: ["1","2","3","4"],
+        correct: "3",
+    },
+]
