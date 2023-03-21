7903b9afa401f25922556580faf7a71de2ddc890
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-03-20 17:42:56 -0700

    For help

--- script.js
+++ script.js
@@ -22,52 +22,53 @@ let questions = [
     {
         question: "Whats my name?",
         options: [
-            {answer:'this', correct: false},
+            {answer:'Bob', correct: false},
             {answer: 'Jonathan', correct: true},
             {answer: 'Jim', correct: false},
-            {answer: 'Ashley', correct: false}
+            {answer: 'James', correct: false}
         ]
     },
      {
-         question: "Whats the sky?",
+         question: "Whats color is the sky?",
          options: [
-             {answer:'', correct: false},
-             {answer: 'Jonathan', correct: true},
-             {answer: 'Jim', correct: false},
-             {answer: 'Ashley', correct: false}
+             {answer:'Green', correct: false},
+             {answer: 'Blue', correct: true},
+             {answer: 'Purple', correct: false},
+             {answer: 'Yellow', correct: false}
          ]
      },
      {
-         question: "Whats the rain?",
+         question: "What state number is Arizona?",
          options: [
-             {answer:'this', correct: false},
-             {answer: 'Jonathan', correct: true},
-             {answer: 'Jim', correct: false},
-             {answer: 'Ashley', correct: false}
+             {answer:'15', correct: false},
+             {answer: '48', correct: true},
+             {answer: '37', correct: false},
+             {answer: '1', correct: false}
          ]
      },
      {
-         question: "Whats my computer?",
+         question: "How much does the average house cost?",
          options: [
-             {answer:'Bob', correct: false},
-             {answer: 'Jonathan', correct: true},
-             {answer: 'Jim', correct: false},
-             {answer: 'Ashley', correct: false}
+             {answer:'300k', correct: false},
+             {answer: 'Too much!', correct: true},
+             {answer: '200k', correct: false},
+             {answer: '700k', correct: false}
          ]
      },
      {
-         question: "Whats the point of life?",
+         question: "What is the meaning of life?",
          options: [
-             {answer:'Bob', correct: false},
-             {answer: 'Jonathan', correct: true},
-             {answer: 'Jim', correct: false},
-             {answer: 'Ashley', correct: false}
+             {answer:'Who knows?', correct: false},
+             {answer: '42', correct: true},
+             {answer: 'Happiness', correct: false},
+             {answer: 'Success', correct: false}
          ]
      },
     
 ]
 /*console.log(questions[0].options[0].answer)*/
 function getQuestion() {
+    
     currentQuestion();
     
 }
@@ -76,9 +77,9 @@ function currentQuestion() {
     questionEl.textContent = questions[questionNumber].question;
     let currentQuestion = questions[questionNumber]
     console.log(currentQuestion);
-    let questionText = currentQuestion.question;
+    //let questionText = currentQuestion.question;
     let options = currentQuestion.options;
-    for(i=0; i<options.length; i++) {
+    for(i = 0; i < options.length; i++) {
         //create button element
         let buttonEl = document.createElement('button')
 
@@ -94,20 +95,47 @@ function currentQuestion() {
             }else {
                 console.log("wrong")
             }
-            
+            questionNumber++;
+           
+            getQuestion(); 
         })
         //append button element to answers list
         answersEl.append(buttonEl)
     }
 }
 
-function userClick() {
+/*function currentQuestion() {
+    questionEl.textContent = questions[questionNumber].question;
+    let currentQuestion = questions[questionNumber]
+    console.log(currentQuestion);
+    let options = currentQuestion.options;
+    for(i = 0; i < options.length; i++) {
+        //create button element
+        
+        //add text to button element
+        answersEl.innerText = options[i].answer
+       
+       
+        let correct = options[i].correct
+        //add event listener to button element
+        buttonEl.addEventListener('click', function(){
+            console.log(correct)
+            if(correct) {
+                score = score + 10
+                console.log("correct")
+            }else {
+                console.log("wrong")
+            }
+            
+            questionNumber++;
+           
+            getQuestion(); 
+        })
+        //append button element to answers list
+        
+    }
+}*/
 
-}
-
-function buttons() {
-    
-}
 
 //I need to check the value of the correct answer
 //If correct, add points to score and move to next question.
